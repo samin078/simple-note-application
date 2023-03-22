8d21de16efe42ae92425af0ca5259e93a0f65e1b
Samin Saiara <saminsaiara@gmail.com>
2023-03-22 20:30:19 +0600

    added file practice with constructor,getter and setter

---
+++ app/src/main/java/com/example/simplenoteapplication/Practice.java
@@ -0,0 +1,17 @@
+package com.example.simplenoteapplication;
+
+public class Practice {
+    String name;
+
+    public Practice(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
